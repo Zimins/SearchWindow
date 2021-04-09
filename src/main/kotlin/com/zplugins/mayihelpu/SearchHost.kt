8263a39577567f4e01d5f3ab754040cd94cedaf9
zimin byun <wlals822@naver.com>
2021-04-09 23:03:52 +0900

    Add host types (stackoverflow, codesearch)
    * Modified original android to android-doc

--- src/main/kotlin/com/zplugins/mayihelpu/SearchHost.kt
+++ src/main/kotlin/com/zplugins/mayihelpu/SearchHost.kt
@@ -6,8 +6,10 @@ enum class SearchHost(val prefix: String, val urlTemplate: String) {
 
     GOOGLE("", "https://www.google.co.kr/search?q=%s"),
     NAVER(":naver", "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%s"),
-    ANDROID(":android", "https://developer.android.com/s/results/?q=%s"),
-    GREPCODE(":grep", "http://grepcode.com/search/?query=%s");
+    ANDROID_DOC(":androiddoc", "https://developer.android.com/s/results/?q=%s"),
+    GREPCODE(":grep", "http://grepcode.com/search/?query=%s"),
+    STACKOVERFLOW(":stack", "https://stackoverflow.com/search?q=%s"),
+    ANDROID_CODE_SEARCH(":androidcode", "https://cs.android.com/search?q=%s");
 
     fun search(input: String) {
         val searchUrl = String.format(urlTemplate, input)
